f515d1984cf7e0b0b11978553fb7a0855d43f741
Michael L Perry <michael@qedcode.com>
2014-05-28 21:48:52 -0500

    Add Correspondence app to both Windows and Windows Phone.

---
+++ IDidIt/IDidIt.Windows/HTTPConfigurationProvider.cs
@@ -0,0 +1,34 @@
+using System.Linq;
+using UpdateControls.Fields;
+using UpdateControls.Correspondence.BinaryHTTPClient;
+using IDidIt.Model;
+
+namespace IDidIt
+{
+    public class HTTPConfigurationProvider : IHTTPConfigurationProvider
+    {
+        private Independent<Individual> _individual = new Independent<Individual>();
+
+        public Individual Individual
+        {
+            get { return _individual; }
+            set { _individual.Value = value; }
+        }
+
+        public HTTPConfiguration Configuration
+        {
+            get
+            {
+                string address = "https://api.facetedworlds.com/correspondence_server_web/bin";
+                string apiKey = "<<Your API key>>";
+				int timeoutSeconds = 30;
+                return new HTTPConfiguration(address, "IDidIt", apiKey, timeoutSeconds);
+            }
+        }
+
+        public bool IsToastEnabled
+        {
+            get { return false; }
+        }
+    }
+}
